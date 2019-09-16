c9be176cb933ed33c25361ce5cc80fef6c2e3edb
ZeroGwafa <zerogwafa@hotmail.com>
2019-09-16 03:55:24 -0400

    Re-wrote a bunch of code to be much more efficient.  Table now dynamically generated, which will make adding additional components easier. Data now stored in an object.  Added collapseable menu.  Added Export to CSV. Tweaked CSS to fix some weird button issues.

---
+++ scripts/data.js
@@ -0,0 +1,312 @@
+var compsList;
+
+if (localStorage.getItem("mats") != null) {
+    compsList = JSON.parse(localStorage.mats)
+} else {
+    compsList = {
+        "Ancient components": {
+            type: "rare",
+            qty: 0
+        },
+        "Armadyl components": {
+            type: "rare",
+            qty: 0
+        },
+        "Ascended components": {
+            type: "rare",
+            qty: 0
+        },
+        "Avernic components": {
+            type: "rare",
+            qty: 0
+        },
+        "Bandos components": {
+            type: "rare",
+            qty: 0
+        },
+        "Brassican components": {
+            type: "rare",
+            qty: 0
+        },
+        "Clockwork components": {
+            type: "rare",
+            qty: 0
+        },
+        "Corporeal components": {
+            type: "rare",
+            qty: 0
+        },
+        "Culinary components": {
+            type: "rare",
+            qty: 0
+        },
+        "Cywir components": {
+            type: "rare",
+            qty: 0
+        },
+        "Dragonfire components": {
+            type: "rare",
+            qty: 0
+        },
+        "Explosive components": {
+            type: "rare",
+            qty: 0
+        },
+        "Faceted components": {
+            type: "rare",
+            qty: 0
+        },
+        "Fortunate components": {
+            type: "rare",
+            qty: 0
+        },
+        "Fungal components": {
+            type: "rare",
+            qty: 0
+        },
+        "Harnessed components": {
+            type: "rare",
+            qty: 0
+        },
+        "Ilujankan components": {
+            type: "rare",
+            qty: 0
+        },
+        "Knightly components": {
+            type: "rare",
+            qty: 0
+        },
+        "Noxious components": {
+            type: "rare",
+            qty: 0
+        },
+        "Oceanic components": {
+            type: "rare",
+            qty: 0
+        },
+        "Pestiferous components": {
+            type: "rare",
+            qty: 0
+        },
+        "Resilient components": {
+            type: "rare",
+            qty: 0
+        },
+        "Rumbling components": {
+            type: "rare",
+            qty: 0
+        },
+        "Saradomin components": {
+            type: "rare",
+            qty: 0
+        },
+        "Seren components": {
+            type: "rare",
+            qty: 0
+        },
+        "Shadow components": {
+            type: "rare",
+            qty: 0
+        },
+        "Shifting components": {
+            type: "rare",
+            qty: 0
+        },
+        "Silent components": {
+            type: "rare",
+            qty: 0
+        },
+        "Undead components": {
+            type: "rare",
+            qty: 0
+        },
+        "Zamorak components": {
+            type: "rare",
+            qty: 0
+        },
+        "Zaros components": {
+            type: "rare",
+            qty: 0
+        },
+        "Dextrous components": {
+            type: "uncommon",
+            qty: 0
+        },
+        "Direct components": {
+            type: "uncommon",
+            qty: 0
+        },
+        "Enhancing components": {
+            type: "uncommon",
+            qty: 0
+        },
+        "Ethereal components": {
+            type: "uncommon",
+            qty: 0
+        },
+        "Evasive components": {
+            type: "uncommon",
+            qty: 0
+        },
+        "Healthy components": {
+            type: "uncommon",
+            qty: 0
+        },
+        "Heavy components": {
+            type: "uncommon",
+            qty: 0
+        },
+        "Imbued components": {
+            type: "uncommon",
+            qty: 0
+        },
+        "Light components": {
+            type: "uncommon",
+            qty: 0
+        },
+        "Living components": {
+            type: "uncommon",
+            qty: 0
+        },
+        "Pious components": {
+            type: "uncommon",
+            qty: 0
+        },
+        "Powerful components": {
+            type: "uncommon",
+            qty: 0
+        },
+        "Precious components": {
+            type: "uncommon",
+            qty: 0
+        },
+        "Precise components": {
+            type: "uncommon",
+            qty: 0
+        },
+        "Protective components": {
+            type: "uncommon",
+            qty: 0
+        },
+        "Refined components": {
+            type: "uncommon",
+            qty: 0
+        },
+        "Sharp components": {
+            type: "uncommon",
+            qty: 0
+        },
+        "Strong components": {
+            type: "uncommon",
+            qty: 0
+        },
+        "Stunning components": {
+            type: "uncommon",
+            qty: 0
+        },
+        "Subtle components": {
+            type: "uncommon",
+            qty: 0
+        },
+        "Swift components": {
+            type: "uncommon",
+            qty: 0
+        },
+        "Variable components": {
+            type: "uncommon",
+            qty: 0
+        },
+        "Base parts": {
+            type: "common",
+            qty: 0
+        },
+        "Blade parts": {
+            type: "common",
+            qty: 0
+        },
+        "Clear parts": {
+            type: "common",
+            qty: 0
+        },
+        "Connector parts": {
+            type: "common",
+            qty: 0
+        },
+        "Cover parts": {
+            type: "common",
+            qty: 0
+        },
+        "Crafted parts": {
+            type: "common",
+            qty: 0
+        },
+        "Crystal parts": {
+            type: "common",
+            qty: 0
+        },
+        "Deflecting parts": {
+            type: "common",
+            qty: 0
+        },
+        "Delicate parts": {
+            type: "common",
+            qty: 0
+        },
+        "Flexible parts": {
+            type: "common",
+            qty: 0
+        },
+        "Head parts": {
+            type: "common",
+            qty: 0
+        },
+        "Magic parts": {
+            type: "common",
+            qty: 0
+        },
+        "Metallic parts": {
+            type: "common",
+            qty: 0
+        },
+        "Organic parts": {
+            type: "common",
+            qty: 0
+        },
+        "Padded parts": {
+            type: "common",
+            qty: 0
+        },
+        "Plated parts": {
+            type: "common",
+            qty: 0
+        },
+        "Simple parts": {
+            type: "common",
+            qty: 0
+        },
+        "Smooth parts": {
+            type: "common",
+            qty: 0
+        },
+        "Spiked parts": {
+            type: "common",
+            qty: 0
+        },
+        "Spiritual parts": {
+            type: "common",
+            qty: 0
+        },
+        "Stave parts": {
+            type: "common",
+            qty: 0
+        },
+        "Tensile parts": {
+            type: "common",
+            qty: 0
+        },
+        "Junk": {
+            type: "common",
+            qty: 0
+        }
+    }
+}
--- scripts/script.js
+++ scripts/script.js
@@ -1,138 +1,143 @@
 var reader = new ChatBoxReader();
-    reader.readargs = 
-        {
-            colors: [
-                a1lib.mixcolor(255,255,255),//Common Mats
-                a1lib.mixcolor(255, 128, 0), //Uncommon Mats
-                a1lib.mixcolor(255, 165, 0), //Scavenging comps
-                a1lib.mixcolor(255, 0, 0) //Rare Mats
-            ],
-            backwards: true
-        };
-    reader.find();
+reader.readargs = {
+    colors: [
+        a1lib.mixcolor(255, 255, 255), //Common Mats
+        a1lib.mixcolor(255, 128, 0), //Uncommon Mats
+        a1lib.mixcolor(255, 165, 0), //Scavenging comps
+        a1lib.mixcolor(255, 0, 0) //Rare Mats
+    ],
+    backwards: true
+};
+reader.find();
 
 var chatCheck = reader.read();
-
-var list = ["Ancient components", "Armadyl components", "Ascended components", "Avernic components", "Bandos components", "Brassican components", "Clockwork components", "Corporeal components", "Culinary components", "Cywir components", "Dragonfire components", "Explosive components", "Faceted components", "Fortunate components", "Fungal components", "Harnessed components", "Ilujankan components", "Knightly components", "Noxious components", "Oceanic components", "Pestiferous components", "Resilient components", "Rumbling components", "Saradomin components", "Seren components", "Shadow components", "Shifting components", "Silent components", "Undead components", "Zamorak components", "Zaros components", "Dextrous components", "Direct components", "Enhancing components", "Ethereal components", "Evasive components", "Healthy components", "Heavy components", "Imbued components", "Light components", "Living components", "Pious components", "Powerful components", "Precious components", "Precise components", "Protective components", "Refined components", "Sharp components", "Strong components", "Stunning components", "Subtle components", "Swift components", "Variable components", "Base parts", "Blade parts", "Clear parts", "Connector parts", "Cover parts", "Crafted parts", "Crystal parts", "Deflecting parts", "Delicate parts", "Flexible parts", "Head parts", "Magic parts", "Metallic parts", "Organic parts", "Padded parts", "Plated parts", "Simple parts", "Smooth parts", "Spiked parts", "Spiritual parts", "Stave parts", "Tensile parts", "Junk"]
-    
-var list2 = new Array();
-        list2.length = list.length;
-        list2.fill(0);
 
 var count, mats, index;
 var actions = 0;
 
-function readChatbox() 
-{
-	var opts 		= reader.read() 		|| [];
+function readChatbox() {
+    var opts = reader.read() || [];
     var chat = "";
     reader.find();
-    
-    for(a in opts)
-    {
-            chat += opts[a].text + " ";
+
+    for (a in opts) {
+        chat += opts[a].text + " ";
     }
-    
-    
+
     var comps = chat.match(/\d+ x \w+( \w+)?[^\d+:]|You receive \d+ \w+( \w+)?[^\d+:]/g);
-	console.log(comps);
-	if(comps != null && comps.length > -1)
-		actions++;
-    for (var x in comps)
-    {
+    console.log(comps);
+    if (comps != null && comps.length > -1)
+        actions++;
+    for (var x in comps) {
         console.log(comps[x]);
         count = Number(comps[x].match(/\d+/)); //1
         mats = comps[x].match(/[^You receive \d]\w+( \w+)?/)[0]; //Junk
-        if(mats.match(/parts|components|Junk/))
+        if (mats.match(/parts|components|Junk/))
             console.log("is fine");
-        else
-        {
+        else {
             mats += "s";
-            console.log("Now "+mats);
+            console.log("Now " + mats);
         }
-        if(list.indexOf(mats) > -1)
-            index = list.indexOf(mats);//Get index of mat based off of list.
-        else
-            continue;
-        list2[index] += Number(count); //add count to index of second list.
-        tidyTable(index);
+        compsList[mats].qty++; //add count to index of second list.
+        tidyTable(mats);
     }
-}    
+}
 
-$("button.tracker").click(function()
-{
-    if($(this).html() == "Start")
-    {
-        console.log("Starting tracker");
-        tracking = setInterval(function(){readChatbox();},600);
-        $(this).html("Stop");
+function buildTable() {
+    for (x in compsList) {
+        if (compsList[x].type === "rare") {
+            $(".rare").append(`<tr data-name="${x}"><td>${x.split(" ")[0]}</td><td class='qty'></td></tr>`);
+        }
+        if (compsList[x].type === "uncommon") {
+            $(".uncommon").append(`<tr data-name="${x}"><td>${x.split(" ")[0]}</td><td class='qty'></td></tr>`);
+        }
+        if (compsList[x].type === "common") {
+            $(".common").append(`<tr data-name="${x}"><td>${x.split(" ")[0]}</td><td class='qty'></td></tr>`);
+        }
     }
-    else
-    {
+}
+
+function tidyTable(flashRow) {
+    localStorage.mats = JSON.stringify(compsList);
+    for (x in compsList) {
+        $(`[data-name='${x}'] > .qty`).text(compsList[x].qty);
+        if (compsList[x].qty === 0) {
+            $(`[data-name='${x}']`).hide();
+        } else {
+            $(`[data-name='${x}']`).show();
+        }
+    }
+    $(`[data-name='${mats}']`).css({ "background-color": "lime" }).animate({
+        backgroundColor: $.Color("rgba(0, 0, 0, 0)")
+    }, 500, function() { $(this).removeAttr("style") });
+
+    $(".actions").text(actions);
+}
+
+buildTable();
+tidyTable();
+
+$(".edit").change(function() {
+    if ($(this).is(':checked')) {
+        if ($(".tracker").text() == "Stop") {
+            $(".tracker").click();
+        }
+        $("tr:hidden").show();
+        $(".qty").attr('contenteditable', 'true').focus(function() { document.execCommand('selectAll', false, null) });
+    } else {
+        $(".qty").removeAttr('contenteditable');
+        for (x in compsList) {
+            compsList[x].qty = parseInt($(`[data-name='${x}'] .qty`).text());
+        }
+        tidyTable();
+    }
+});
+
+$("button.tracker").click(function() {
+    if ($(this).html() == "Start") {
+        console.log("Starting tracker");
+        tracking = setInterval(function() { readChatbox(); }, 600);
+        $(this).html("Stop");
+    } else {
         console.log("Stopping tracker");
         $(this).html("Start");
         clearInterval(tracking);
     }
 }).click();
 
-$("button.clear").click(function()
-{
-    list2.fill(0);
-	actions = 0;
-    tidyTable();
+$("button.clear").click(function() {
+    localStorage.removeItem("mats");
+    for (x in compsList) {
+        compsList[x].qty = 0;
+    }
+    actions = 0;
+    location.reload();
 });
-    
-function tidyTable(index)
-{
-    localStorage.mats = list2;
-    $("table:contains('Comps') tr:not(:first)").hide();
-    $("tr:not(:first) > td:nth-child(2)").each(function(e)
-    {
-        $(this).text(list2[e]);//update all table rows.
-    });
-    
-    for(x in list2)
-    {
-        if(list2[x] != 0)
-        {
-          $("table:contains('Comps') tr:not(:first)").eq(x).show();
+
+$(".toggleMenu").click(function() {
+    $(".options").toggle();
+});
+
+$(".export").click(function() {
+    var str = 'ComponentName,Quantity\n'; // column headers
+    for (x in compsList) {
+        str = str + x + "," + compsList[x].qty + "\n"
+    }
+    //return str;
+    var blob = new Blob([str], { type: 'text/csv;charset=utf-8;' });
+    if (navigator.msSaveBlob) { // IE 10+
+        navigator.msSaveBlob(blob, "componentExport.csv");
+    } else {
+        var link = document.createElement("a");
+        if (link.download !== undefined) { // feature detection
+            // Browsers that support HTML5 download attribute
+            var url = URL.createObjectURL(blob);
+            link.setAttribute("href", url);
+            link.setAttribute("download", "componentExport.csv");
+            link.style.visibility = 'hidden';
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
         }
-    }
-    $("table:contains('Comps') tr:not(:first)").eq(index).css({"background-color":"lime"}).animate({
-    backgroundColor:$.Color( "rgba(0, 0, 0, 0)")},500, function(){$(this).removeAttr("style")});
-	$(".actions").text(actions);
-}
-
-
-if(localStorage.getItem("mats") != null)
-{
-    for(x in localStorage.mats.split(','))
-    {
-        list2[x] = Number(localStorage.mats.split(',')[x]);
-    }
-}
-else
-{
-    list2.fill(0);
-}
-tidyTable();
-
-$(".edit").change(function()
-{
-    if($(this).is(':checked'))
-    {
-        if($(".tracker").text() == "Stop")
-        {
-            $(".tracker").click();
-        }
-        $("tr:hidden").show();
-        $("table:last tr").find("td:last").attr('contenteditable','true').focus(function(){document.execCommand('selectAll',false,null)});
-    }
-    else
-    {
-        $("table:last tr td").removeAttr('contenteditable');
-        list2 = [];
-        $("table:last tr").find("td:last").each(function(){list2.push(parseInt($(this).text()) || 0)})
-        tidyTable();
     }
 });
